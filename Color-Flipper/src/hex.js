fb3323fcd690f9f9a2382e915a8bf28e2ed45800
Subhadeep <subhadeeppaul35@gmail.com>
2023-03-25 21:24:21 +0530

    🎮 Counter Project added

--- Color-Flipper/src/hex.js
+++ Color-Flipper/src/hex.js
@@ -1,15 +1,20 @@
+ //array of values for the background color
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
 btn.addEventListener("click", function () {
+    // saves the new background color
   let hexColor = "#";
+  // randomly picks a value in the array and adds it to the background color above
   for (let i = 0; i < 6; i++) {
     hexColor += hex[getRandomNumber()];
   }
   // console.log(hexColor);
 
+  // changes the text inside the button
   color.textContent = hexColor;
+  // changes the background color
   document.body.style.backgroundColor = hexColor;
 });
 
