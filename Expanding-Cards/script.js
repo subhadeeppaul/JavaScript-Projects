3950bb7eaf638de053ed1feed74399ff9ca8b7fe
Subhadeep <subhadeeppaul35@gmail.com>
2023-06-13 14:27:40 +0530

    code changes

--- Expanding-Cards/script.js
+++ Expanding-Cards/script.js
@@ -1,14 +1,16 @@
 const panels = document.querySelectorAll(".panel");
 
+// Add click event listener to each panel
 panels.forEach((panel) => {
   panel.addEventListener("click", () => {
-    removeActiveClasses();
-    panel.classList.add("active");
+    removeActiveClasses(); // Remove active class from all panels
+    panel.classList.add("active"); // Add active class to the clicked panel
   });
 });
 
+// Function to remove active classes from all panels
 function removeActiveClasses() {
   panels.forEach((panel) => {
-    panel.classList.remove("active");
+    panel.classList.remove("active"); // Remove active class from each panel
   });
 }
