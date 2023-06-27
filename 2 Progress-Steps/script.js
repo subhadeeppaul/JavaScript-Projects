d507c9e65da65a7d25b5224a82b56be0dd25831c
Subhadeep <subhadeeppaul35@gmail.com>
2023-06-27 06:30:16 +0530

    New code added

--- 2 Progress-Steps/script.js
+++ 2 Progress-Steps/script.js
@@ -1,12 +1,11 @@
-// Get references to the necessary DOM elements
 const progress = document.getElementById("progress");
-const prev = document.getElementById("prev");
 const next = document.getElementById("next");
+const prev = document.getElementById("prev");
+
 const circles = document.querySelectorAll(".circle");
 
-let currentActive = 1; // Current active step
+let currentActive = 1;
 
-// Arrow function to update the progress bar and circles
 const update = () => {
   circles.forEach((circle, idx) => {
     if (idx < currentActive) {
@@ -17,29 +16,37 @@ const update = () => {
   });
 
   const activeSteps = document.querySelectorAll(".active");
-
   const progressWidth =
     ((activeSteps.length - 1) / (circles.length - 1)) * 100 + "%";
+
   progress.style.width = progressWidth;
 
-  prev.disabled = currentActive === 1;
-  next.disabled = currentActive === circles.length;
+  if (currentActive === 1) {
+    prev.disabled = true;
+  } else if (currentActive === circles.length) {
+    next.disabled = true;
+  } else {
+    prev.disabled = false;
+    next.disabled = false;
+  }
 };
 
-// Event listener for the next button
 next.addEventListener("click", () => {
   currentActive++;
+
   if (currentActive > circles.length) {
     currentActive = circles.length;
   }
+
   update();
 });
 
-// Event listener for the previous button
 prev.addEventListener("click", () => {
   currentActive--;
+
   if (currentActive < 1) {
     currentActive = 1;
   }
+
   update();
 });
